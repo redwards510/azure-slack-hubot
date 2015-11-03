344cf5f539ec6fa17aaa0ef1e27134052a9d14f7
Ryan Edwards <redwards@onelegal.com>
2015-11-02 16:35:07 -0800

    added meme generator

--- server.js
+++ server.js
@@ -1,2 +1,4 @@
 require('coffee-script/register');
+process.env.HUBOT_MEMEGEN_USERNAME = 'redwards';
+process.env.HUBOT_MEMEGEN_PASSWORD = '9Q#vtKdEKIbs';
 module.exports = require('hubot/bin/hubot.coffee');
